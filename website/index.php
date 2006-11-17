dd9c12c5d9670eed6bb80472d81936a023b57bdc
akitabright <akitabright@53a72767-d613-0410-9012-a394e7552fcb>
2006-11-17 17:36:22 +0000

    Basic lame website
    git-svn-id: svn://svn.code.sf.net/p/ajaxometer/code@15 53a72767-d613-0410-9012-a394e7552fcb

---
+++ website/index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+    <title>AJAXOmeter</title>
+  </head>
+  <style>
+    body {
+      background-color: #FFFFFF;
+    }
+    img.sf_logo {
+      position: absolute;
+      bottom: 0px;
+      right: 0px;
+    }
+    img.lobo_logo {
+      position: absolute;
+      bottom: 0px;
+      left: 0px;
+    }
+  </style>
+  <body>
+    <center>
+    <h2>
+      Sorry we havn't made a snazy looking website yet. If you want to see a demo, which I'm sure you do,
+    <h2>
+    <h1>
+      <a href="ajaxometer.php">click here for the demo</a>
+    <h1/>
+    <br/>
+    <br/>
+    <br/>
+    <h2/>
+      Once you know you want to download this fine bit set of code
+    </h2>
+    <h1>
+      <a href="http://sourceforge.net/project/showfiles.php?group_id=166297">download it from here</a>
+    </h1>
+    </center>
+    
+    <br/><br/>
+    <br/><br/>
+    <a href="http://sourceforge.net"><img class='sf_logo' src="http://sflogo.sourceforge.net/sflogo.php?group_id=166297&amp;type=2" width="125" height="37" border="0" alt="SourceForge.net Logo" /></a> 
+    <a href="http://lobo.net"><img class='lobo_logo' src="lobo_logo.gif" width="125" height="37" border="0" alt="Lobo.net Logo" /></a> 
+  </body>
+</html>
